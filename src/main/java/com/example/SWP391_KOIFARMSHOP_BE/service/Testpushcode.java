2da5474770038759d1b34e0335b13725c8d43966
= <=>
2025-01-18 14:54:08 +0700

    service

---
+++ src/main/java/com/example/SWP391_KOIFARMSHOP_BE/service/Testpushcode.java
@@ -0,0 +1,20 @@
+package com.example.SWP391_KOIFARMSHOP_BE.service;
+
+import com.example.SWP391_KOIFARMSHOP_BE.model.ProductRequest;
+import com.example.SWP391_KOIFARMSHOP_BE.model.ProductResponse;
+import com.example.SWP391_KOIFARMSHOP_BE.pojo.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Testpushcode {
+    @Autowired
+    private Product product;
+    public ProductResponse test(ProductRequest productRequest){
+        ProductResponse p = new ProductResponse();
+        if(productRequest.getBreed().isEmpty()){
+            System.out.println("Rong");
+        }
+        return p;
+    }
+}
